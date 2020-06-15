a7ef7b5de38ba35b2a59f8b24b199ce39b6edd45
olgab <olgabresheva7@gmail.com>
2020-06-14 19:25:55 -0700

    #added taskDeleteAlertConfirm file

---
+++ src/TaskDeleteAlertConfirm.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+
+function TaskDeleteAlertConfirm(props) {
+
+    const deleteIcon = (<svg className="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
+                             xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fillRule="evenodd"
+              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+    </svg>);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <div>
+
+        <span className="float-left"
+              onClick={handleShow}>{deleteIcon}</span>
+
+            <>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Body>Please confirm if you wish to delete this task?</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary btn-sm" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary btn-sm" onClick={() => props.onTaskDelete(props.task.id)}>
+                            Confirm
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        </div>
+
+    );
+}
+
+export default TaskDeleteAlertConfirm;
